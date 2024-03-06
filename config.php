426ab41b09e88c44065328066ffe1bcb5727281f
Matteo Nardin <nardin.matteo@gmail.com>
2024-03-06 10:53:30 +0100

    patch

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+    return [
+        'driver' => 'mysql', //postgres
+        'host' => 'localhost',
+        'database' => 'pdo_test', //database effettivo?
+
+        // 'port'=> '3306',
+        'user' => 'root',
+        'password' => '',
+        'dsn' => "mysql:host=localhost; database="
+    ];
